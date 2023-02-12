5f9de65091c4f4350e864e2f317631f838017cd6
Julio Rivas <rivasjulio@ymail.com>
2023-02-11 21:48:23 -0500

    COMPLETE!

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name:"Julio",
+    streetAddress: "mcdonald",
+
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress, mcdonald) {
+const newEmployee = {...employee};
+newEmployee.name = "Sam";
+newEmployee.streetAddress = "11 Broadway";
+
+return newEmployee;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress,mcdonald) {
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+
+}
+function deleteFromEmployeeByKey(employee, streetAddress, mcdonald) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, streetAddress, mcdonald) {
+    delete employee.name;
+    return employee;
+    
+}
